ec77baa5f518984e9a5797605c6af2244f9ae5e0
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-10-30 20:44:22 +0530

    Adding some comments in the selectionSort

--- src/SortingAlgorithms/SelectionSort.js
+++ src/SortingAlgorithms/SelectionSort.js
@@ -4,12 +4,15 @@ function selectionSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     let minIndex = i;
 
+    // This for loops runs through the whole array
+    // and determines the min element in the array
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[minIndex]) {
         minIndex = j;
       }
     }
 
+    // Swaps the min element withe the first element in the unsorted part
     const temp = arr[i];
     arr[i] = arr[minIndex];
     arr[minIndex] = temp;
